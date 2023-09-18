f272854f8810e66e7f58436aca31ee827a3ce49b
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-09-17 21:40:08 -0300

    new phrase has been added to the landing page

--- client/src/app/components/logo.tsx
+++ client/src/app/components/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Logo(){
     return(
         <div className="flex flex-shrink-0 items-center">
-            <Link className='text-yellow-400 rounded-md text-md hover:bg-blue-800  bg-blue-700 p-1 font-bold uppercase'  href={'/'} >Soccer-bet</Link>
+            <Link className='text-yellow-400 rounded-md text-md hover:bg-blue-800  bg-gray-800 p-1 font-bold uppercase'  href={'/'} >Soccer-bet</Link>
         </div>
     )
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -10,39 +10,65 @@ export default function Home() {
   const [loggedIn, setLoggedIn] = useState(false)
   
   return(
-    <section className='relative bg-gray-900 h-screen flex justify-center items-start'  >
-        <Image className='absolute top-0 left-0  md:shrink-0' src={image1} alt='Decoration image' />
-      
+    <main>
+      <section className='relative bg-gray-900 h-screen flex justify-center items-start'  >
+          <Image className='absolute top-0 left-0  md:shrink-0' src={image1} alt='Decoration image' />
+        
+        <div className='absolute z-10 bg-yellow-500 content flex flex-col justify-items items-center gap-4 rounded-md p-12'>
+          <h1 className='text-center inline text-gray-100 rounded-md text-5xl bg-gray-800 p-3 font-bold uppercase'>
+            SoccerBets
+          </h1>
+          <h2 className="font-bold text-md text-2xl text-gray-800 p-3" >Play with your friends and family</h2>
+          <p className="font-bold text-md text-2xl text-gray-800 w text-justify" style={{width:'50%'}} >
+          Are you a die-hard football enthusiast seeking an all-in-one platform for the latest updates, insightful analysis, and interactive gaming experiences? Look no further than SoccerBets – your go-to app for all things football!
+          </p>
 
-      <div className='absolute z-10 bg-yellow-500 content flex flex-col justify-items items-center gap-4 rounded-md p-12'>
-        <h1 className='text-center inline text-yellow-400 rounded-md text-5xl bg-blue-700 p-1 font-bold uppercase'>
-          SoccerBets
-        </h1>
-        <h2 className="font-bold text-md text-2xl text-gray-800 p-3" >Play with your friends and family</h2>
-
-        <div>
-          <h3>Content that we provide</h3>
-          <div className='grid grid-cols-2 gap-4' >
-            <div className='bg-gray-800 p-5 rounded-md text-lg'>
-              
-              
-              
-              Championships</div>
-            <div className='bg-gray-800 p-5 rounded-md text-lg'>
-              
-
-              Times
+          {/* <div>
+            <h3>Content that we provide</h3>
+            <div className='grid grid-cols-2 gap-4' >
+              <div className='bg-gray-800 p-5 rounded-md text-lg'>
+                <p className="text-center">Championships</p>
+              </div>
+              <div className='bg-gray-800 p-5 rounded-md text-lg'>
+                <p className="text-center">Teams</p>
+              </div>
+              <div className='bg-gray-800 p-5 rounded-md text-lg'>
+                <p className="text-center">Players</p>
+              </div>
+              <div className='bg-gray-800 p-5 rounded-md text-lg'>
+              <p className="text-center" >Games</p>
+              </div>
             </div>
-            <div className='bg-gray-800 p-5 rounded-md text-lg'>Jogadores</div>
-            <div className='bg-gray-800 p-5 rounded-md text-lg'>Jogos</div>
-          </div>
+          </div> */}
+
+          <Link href="/login" className="text-2xl bg-green-700 hover:bg-green-600 p-5 w-100 rounded-md ">
+            Learn more
+          </Link>
+
         </div>
+    
+      </section>
 
-        <Link href="/login" className="text-2xl bg-gray-800 p-5 w-50 rounded-full">Start now</Link>
-
-      </div>
-  
-    </section>
+      <section className="relative information bg-yellow-500 flex justify-center items-start h-[80vh]" >
+            <div className='flex flex-col gap-5' >
+              <h3 className="text-yellow-400 text-3xl font-bold text-center" >Content that we provide</h3>
+              <div className='grid grid-cols-2 gap-4' >
+                <div className='bg-yellow-400 p-5 rounded-md text-lg'>
+                  <p className="text-center text-gray-900 font-bold">Championships</p>
+                </div>
+                <div className='bg-yellow-400 p-5 rounded-md text-lg'>
+                  <p className="text-center text-gray-900 font-bold">Teams</p>
+                </div>
+                <div className='bg-yellow-400 p-5 rounded-md text-lg'>
+                  <p className="text-center text-gray-900 font-bold">Players</p>
+                </div>
+                <div className='bg-yellow-400 p-5 rounded-md text-lg'>
+                <p className="text-center text-gray-900 font-bold" >Games</p>
+                </div>
+              </div>
+            </div>
+      </section>
+    </main>
   )
 
 }
